1577f2a9b2f8b728e6e054e2a92edbf782c8eb89
lanliwz <lanliwz@yahoo.com>
2024-12-17 17:29:50 -0500

    add test

--- onto2schema/get_schema_test.py
+++ onto2schema/get_schema_test.py
@@ -5,8 +5,21 @@ from cypher_statement.get_schema import *
 start_node ='asset'
 db = SemanticGraphDB(neo4j_bolt_url ,username,password,neo4j_db_name)
 
-start_nodes = db.get_node2node_relationship(start_node)
+nodes_rels = db.get_node2node_relationship(start_node)
+
+for n in nodes_rels:
+    print(n)
+
+end_nodes = db.get_end_nodes(start_node)
+
+for n in end_nodes:
+    print(n)
+
+start_nodes = db.get_start_nodes(start_node)
 
 for n in start_nodes:
     print(n)
 
+rels = db.get_relationships(start_node)
+for n in rels:
+    print(n)
