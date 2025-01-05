9fbd1aaad1378c5ff4a976eb055b2d11e22d2a5a
lanliwz <lanliwz@yahoo.com>
2025-01-04 19:35:09 -0500

    refactor

--- onto2schema/get_schema_test.py
+++ onto2schema/get_schema_test.py
@@ -6,13 +6,14 @@ from cypher_statement.get_schema import *
 db = SemanticGraphDB(neo4j_bolt_url,username,password,neo4j_db_name)
 
 def get_schema(start_node:str):
-    schema = ("\n".join(db.get_node2node_relationship(start_node))
-              + "\n".join(db.get_end_nodes(start_node))
-              + "\n".join(db.get_start_nodes(start_node))
-              + "\n".join(db.get_relationships(start_node))
+    schema = ("\n".join(db.get_node2node_relationship(start_node)) + '\n'
+              + "\n".join(db.get_end_nodes(start_node)) + '\n'
+              + "\n".join(db.get_start_nodes(start_node)) + '\n'
+              + "\n".join(db.get_relationships(start_node)) + '\n'
               )
 
     return schema
 
-# start_node ='person'
-# print(get_schema(start_node))
+start_node ='actor'
+    # 'person'
+print(get_schema(start_node))
