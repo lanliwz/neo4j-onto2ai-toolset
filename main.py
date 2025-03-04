70d959a0637fec6a9a31f3d5093bac193588ce56
lanliwz <lanliwz@yahoo.com>
2025-03-04 16:43:31 -0500

    refactor

---
+++ main.py
@@ -0,0 +1,7 @@
+from neo4j_onto2ai_toolset.schema_chatbot.chatbot import lg as mygraph
+
+def main():
+    mygraph.invoke({"init": "start the process"})
+
+if __name__ == "__main__":
+    main()
--- neo4j_onto2ai_toolset/main.py
+++
@@ -1,7 +0,0 @@
-from schema_chatbot.chatbot import lg
-
-def main():
-    lg.invoke({"init": "start the process"})
-
-if __name__ == "__main__":
-    main()
