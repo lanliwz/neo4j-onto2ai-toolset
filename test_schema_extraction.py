2c2ea755bb49cd8f721cfb7f6411d9ed11eca564
lanliwz <lanliwz@yahoo.com>
2026-01-21 20:30:43 -0500

    refactor loader to load FIBO by domain FND_DOMAIN,BE_DOMAIN,BP_DOMAIN,FBC_DOMAIN

--- test_schema_extraction.py
+++ test_schema_extraction.py
@@ -3,7 +3,7 @@ import json
 from neo4j_onto2ai_toolset.onto2ai_mcp import get_materialized_schema
 
 async def main():
-    result = await get_materialized_schema("person")
+    result = await get_materialized_schema("account")
     print(json.dumps(result, indent=2))
 
 if __name__ == "__main__":
